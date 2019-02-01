ec89a218cb77d00f35bb8259e19a9f9a40746e8b
adrianpraja <adrian@databank.ro>
2019-02-01 23:54:58 +0200

    CreateStack save parameters

--- src/cf.js
+++ src/cf.js
@@ -1,23 +1,84 @@
+//const YAML = require('yaml')
+var yaml = require('js-yaml')
+
+
 module.exports = {
 	CreateStack: function(_POST, cb ) {
 		var account_id = '000000000000'
 		var stack_id   = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[x]/g, function(c) { var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8); return v.toString(16); });
+		var template;
 
-		DynamoDB
-			.table('cloudformation_stacks')
-			.insert_or_replace({
-				account_id: account_id,
-				stack_id: stack_id,
-				name: _POST.StackName,
-				created_at: new Date().getTime(),
-			}, function(err,data) {
-				if (err)
-					return cb(err)
+		async.waterfall([
 
-				cb(null, { StackId: stack_id})
-			})
+			// create stack in db
+			function( cb ) {
+				DynamoDB
+					.table('cloudformation_stacks')
+					.insert_or_replace({
+						account_id: account_id,
+						stack_id: stack_id,
+						name: _POST.StackName,
+						created_at: new Date().getTime(),
+					}, function(err,data) {
+						if (err)
+							return cb(err)
 
-		//console.log("create stack", console.log('CreateStack', _POST))
+						cb()
+					})
+			},
+
+			function( cb ) {
+				template = yaml.safeLoad( _POST.TemplateBody )
+
+				//yml = YAML.parse( _POST.TemplateBody )
+
+
+				cb()
+			},
+
+
+			function( cb ) {
+				if ( !template.hasOwnProperty('Parameters'))
+					return cb()
+
+				if (typeof template.Parameters !== 'object' )
+					return cb()
+
+				async.each(Object.keys(template.Parameters), function( p, cb) {
+					// @todo: add default value
+
+					var value = null
+					if ( template.Parameters[p].hasOwnProperty('Default') )
+						value = template.Parameters[p].Default;
+
+					DynamoDB
+						.table('cloudformation_stack_parameters')
+						.insert_or_replace({
+							stack_id: stack_id,
+							key: p,
+							value: value,
+						}, function( err ) {
+							cb(err)
+						})
+				}, function( err ) {
+					cb()
+				} )
+			},
+
+			//console.log("create stack", console.log('CreateStack', _POST))
+			//console.log(JSON.stringify(yml,null,"\t"))
+		], function( err ) {
+			if (err)
+				return cb(err)
+
+			cb(null, { StackId: stack_id})
+
+		})
+
+
+
+
+
 
 	}
 }
