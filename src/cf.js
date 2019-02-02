57208b965da7ed3cd3765c630e8b8d3965da4d38
adrianpraja <adrian@databank.ro>
2019-02-02 23:14:03 +0200

    update

--- src/cf.js
+++ src/cf.js
@@ -8,6 +8,14 @@ module.exports = {
 		var stack_id   = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[x]/g, function(c) { var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8); return v.toString(16); });
 		var template;
 
+		form_parameters.extract_param('Parameters', _POST )
+		form_parameters.extract_param('NotificationARNs', _POST )
+
+		//console.log( "CreateStack", JSON.stringify(_POST))
+
+		if (!_POST.hasOwnProperty('Parameters'))
+			_POST.Parameters = []
+
 		async.waterfall([
 
 			// create stack in db
@@ -51,6 +59,16 @@ module.exports = {
 					if ( template.Parameters[p].hasOwnProperty('Default') )
 						value = template.Parameters[p].Default;
 
+					// override parameter values with supplied values
+					_POST.Parameters.map(function(pp) {
+						if ( pp.ParameterKey === p )
+							value = pp.ParameterValue
+
+						return pp;
+					})
+
+
+
 					DynamoDB
 						.table('cloudformation_stack_parameters')
 						.insert_or_replace({
@@ -65,11 +83,16 @@ module.exports = {
 				} )
 			},
 
+
+
+
+
 			//console.log("create stack", console.log('CreateStack', _POST))
 			//console.log(JSON.stringify(yml,null,"\t"))
 		], function( err ) {
 			if (err)
 				return cb(err)
+
 
 			cb(null, { StackId: stack_id})
 
