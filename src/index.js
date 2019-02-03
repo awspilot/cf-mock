d0d2c5751bdc2b63aa07e6c752e3af1f39dd85d5
adrianpraja <adrian@databank.ro>
2019-02-03 14:03:48 +0200

    -xml +ractive

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var qs = require('qs')
 form_parameters = require('./lib/form_parameters')
 async=require('async')
 var AWS = require('aws-sdk')
-var xml = require('xml');
+
 
 const DynamodbFactory = require('@awspilot/dynamodb')
 DynamoDB = new DynamodbFactory(
