c488aa654cc8b4f042f4aa230fa1ed331a085e5a
adrianpraja <adrian@databank.ro>
2019-02-04 09:16:36 +0200

    dynamodb create table

---
+++ src/Services/AWS/DynamoDB/Table/create.js
@@ -0,0 +1,49 @@
+
+
+module.exports = function(stack_id, res_name, type, properties, cb ) {
+	console.log( res_name )
+
+	async.waterfall([
+
+
+		// create resource
+		function( cb ) {
+			DynamoDB
+				.table('cloudformation_resources')
+				.insert_or_replace({
+					stack_id: stack_id,
+					resource_name: res_name,
+					type: type,
+					properties: properties,
+					// status in progress
+				}, function(err) {
+					cb(err)
+				} )
+
+		},
+
+		// actually create the table
+		function( cb ) {
+			console.log('creating table ', properties)
+			ClientsDynamoDB.client.createTable({
+				TableName: properties.TableName,
+				AttributeDefinitions: properties.AttributeDefinitions,
+				KeySchema: properties.KeySchema,
+				ProvisionedThroughput: properties.ProvisionedThroughput,
+				GlobalSecondaryIndexes: properties.GlobalSecondaryIndexes,
+			}, function(err) {
+				if (err) console.log(err)
+				cb(err)
+			})
+		},
+		//
+		//
+		// // update status for resource to complete
+		// function( cb ) {
+		// 	cb()
+		// },
+
+	], function(err) {
+		cb(err)
+	})
+}
