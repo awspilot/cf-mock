bf15cf6ef0cc69403477381c16d71815d376e356
adrianpraja <adrian@databank.ro>
2019-02-04 20:39:45 +0200

    delete dynamodb table

---
+++ src/Services/AWS/DynamoDB/Table/delete.js
@@ -0,0 +1,33 @@
+
+module.exports = function(stack_id, res_name, type, properties, cb ) {
+	//console.log( res_name )
+
+	async.waterfall([
+		// @todo: implement Retain Policy
+
+		// delete the table
+		function( cb ) {
+			ClientsDynamoDB.client.deleteTable({ TableName: properties.TableName }, function(err) {
+				cb(err)
+			} )
+		},
+
+		// delete the resource from db
+		function( cb ) {
+			DynamoDB
+				.table('cloudformation_resources')
+				.where('stack_id').eq(stack_id)
+				.where('resource_name').eq(res_name)
+				.delete( function(err) {
+					cb(err)
+				} )
+
+		},
+
+
+	], function(err) {
+		// if err, mark resource as failed delete
+
+		cb(err)
+	})
+}
