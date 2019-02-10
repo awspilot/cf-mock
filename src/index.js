d3ac4a48060855d464b1fe66947cd3cb48036394
adrianpraja <adrian@databank.ro>
2019-02-10 14:56:39 +0200

    region prefix

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ async.waterfall([
 						endpoint:        process.env.CF_DYNAMODB_ENDPOINT,
 						accessKeyId:     process.env.CF_DYNAMODB_KEY,
 						secretAccessKey: process.env.CF_DYNAMODB_SECRET,
-						region:          region,
+						region:          (process.env.CF_DYNAMODB_REGION_PREFIX || '') + region,
 					})
 				)
 
@@ -66,7 +66,7 @@ async.waterfall([
 						endpoint:        process.env.DYNAMODB_ENDPOINT,
 						accessKeyId:     process.env.DYNAMODB_KEY,
 						secretAccessKey: process.env.DYNAMODB_SECRET,
-						region:          (process.env.CF_DYNAMODB_REGION_PREFIX || '') + region,
+						region:          region,
 					})
 				)
 
