2eaf328c5e82fdf8faa9c1cf6ce05f59c91aabc1
adrianpraja <adrian@databank.ro>
2019-02-16 19:59:17 +0200

    update

--- test/tests/002-dynamodb.js
+++ test/tests/002-dynamodb.js
@@ -4,7 +4,7 @@
 
 describe('dynamodb', function () {
 
-	it('!Ref TableName', function(done) {
+	it('!Ref TableName (no parameter value)', function(done) {
 
 		var params = {
 			StackName: 'STRING_VALUE',
@@ -49,7 +49,7 @@ Resources:
 
 			setTimeout(function() {
 				done()
-			}, 5000)
+			}, 3000)
 		});
 	})
 
@@ -74,5 +74,73 @@ Resources:
 
 
 
+
+
+
+
+
+
+	it('!Ref TableName', function(done) {
+
+		var params = {
+			StackName: 'STRING_VALUE',
+			Parameters: [
+				{
+					ParameterKey: 'TableName',
+					ParameterValue: 'my_table',
+				},
+			],
+			TemplateBody: `
+AWSTemplateFormatVersion: 2010-09-09
+Parameters:
+    TableName:
+        Type: String
+        Default: default_table_name
+    OrgDomain:
+        Type: String
+Resources:
+
+    MyTable:
+        Type: AWS::DynamoDB::Table
+        Properties:
+            TableName: !Ref TableName
+            AttributeDefinitions:
+                -
+                  AttributeName: field
+                  AttributeType: S
+
+            KeySchema:
+                -
+                  AttributeName: field
+                  KeyType: HASH
+
+            ProvisionedThroughput:
+                ReadCapacityUnits: 1
+                WriteCapacityUnits: 1
+`,
+		};
+		cloudformation.createStack(params, function(err, data) {
+			if (err)
+				throw err()
+
+			setTimeout(function() {
+				done()
+			}, 3000)
+		});
+	})
+
+	// @todo: list tables, check it it exists
+
+	it('DeleteStack', function(done) {
+		var params = {
+			StackName: 'STRING_VALUE',
+		};
+		cloudformation.deleteStack(params, function(err, data) {
+			if (err)
+				throw err()
+
+			done()
+		});
+	})
 
 })
