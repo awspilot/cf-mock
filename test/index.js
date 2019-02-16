ed6e6a4f77e2097ce1db5f8507646fd93c57ceac
adrianpraja <adrian@databank.ro>
2019-02-16 19:56:34 +0200

    update

--- test/index.js
+++ test/index.js
@@ -4,3 +4,10 @@ require('./lib/index.js')
 
 require('./tests/000-index.js')
 require('./tests/001-ref.js')
+
+require('./tests/002-dynamodb.js')
+
+//require('./tests/003-all.js')
+//require('./tests/002-getTemplateSummary.js')
+//require('./tests/003-getTemplate.js')
+//require('./tests/004-params.js')
---
+++ test/tests/002-dynamodb.js
@@ -0,0 +1,78 @@
+
+
+
+
+describe('dynamodb', function () {
+
+	it('!Ref TableName', function(done) {
+
+		var params = {
+			StackName: 'STRING_VALUE',
+			// Parameters: [
+			// 	{
+			// 		ParameterKey: 'TableName',
+			// 		ParameterValue: 'my_table',
+			// 	},
+			// ],
+			TemplateBody: `
+AWSTemplateFormatVersion: 2010-09-09
+Parameters:
+    TableName:
+        Type: String
+        Default: default_table_name
+    OrgDomain:
+        Type: String
+Resources:
+
+    MyTable:
+        Type: AWS::DynamoDB::Table
+        Properties:
+            TableName: !Ref TableName
+            AttributeDefinitions:
+                -
+                  AttributeName: field
+                  AttributeType: S
+
+            KeySchema:
+                -
+                  AttributeName: field
+                  KeyType: HASH
+
+            ProvisionedThroughput:
+                ReadCapacityUnits: 1
+                WriteCapacityUnits: 1
+`,
+		};
+		cloudformation.createStack(params, function(err, data) {
+			if (err)
+				throw err()
+
+			setTimeout(function() {
+				done()
+			}, 5000)
+		});
+	})
+
+	// @todo: list tables, check it it exists
+
+	it('DeleteStack', function(done) {
+		var params = {
+			StackName: 'STRING_VALUE',
+		};
+		cloudformation.deleteStack(params, function(err, data) {
+			if (err)
+				throw err()
+
+			done()
+		});
+	})
+
+
+
+
+
+
+
+
+
+})
