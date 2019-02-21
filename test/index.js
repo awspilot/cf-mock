35d1a8bc40a035a9bdae09d40ee6eb699359d7c0
adrianpraja <adrian@databank.ro>
2019-02-21 17:54:05 +0200

    update

--- test/index.js
+++ test/index.js
@@ -9,8 +9,3 @@ require('./tests/003-pseudoparameters.js')
 require('./tests/004-liststackresources.js')
 
 require('./tests/020-dynamodb.js')
-
-//require('./tests/003-all.js')
-//require('./tests/002-getTemplateSummary.js')
-//require('./tests/003-getTemplate.js')
-//require('./tests/004-params.js')
