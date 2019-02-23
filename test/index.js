f5e0205a34035d168d2d8ee4125af6365f6cc9cc
adrianpraja <adrian@databank.ro>
2019-02-23 19:35:04 +0200

    update

--- test/index.js
+++ test/index.js
@@ -2,10 +2,13 @@ require("../src/index")
 require('./lib/index.js')
 
 
-require('./tests/000-index.js')
-require('./tests/001-ref.js')
-require('./tests/002-describestacks.js')
-require('./tests/003-pseudoparameters.js')
-require('./tests/004-liststackresources.js')
+// require('./tests/000-index.js')
+// require('./tests/001-ref.js')
+// require('./tests/002-describestacks.js')
+require('./tests/002-getTemplateSummary.js')
+// require('./tests/003-pseudoparameters.js')
+// require('./tests/004-liststackresources.js')
+//
+// require('./tests/020-dynamodb.js')
 
-require('./tests/020-dynamodb.js')
+//require('./tests/xxx-all.js')
