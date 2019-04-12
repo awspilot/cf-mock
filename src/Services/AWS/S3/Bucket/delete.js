c2f77452f547c4fdcf695c44461cb5982b43a9ce
databank <adrian@databank.ro>
2019-04-12 11:12:43 +0300

    update

--- src/Services/AWS/S3/Bucket/delete.js
+++ src/Services/AWS/S3/Bucket/delete.js
@@ -1,10 +1,45 @@
+var AWS = require('aws-sdk')
+var s3  = new AWS.S3({
+	endpoint: 'http://localhost/v1/s3/',
+	sslEnabled: false,
+	s3ForcePathStyle: true,
+	region: 'us-east-1',
+	credentials: {
+		accessKeyId: 'S3RVER',
+		secretAccessKey: 'S3RVER',
+	}
+})
 
 module.exports = function(DynamoDB, ClientsDynamoDB, stack_id, res_name, type, properties, cb ) {
 	//console.log( res_name )
 
-	DynamoDB
-		.table('cloudformation_resources')
-		.where('stack_id').eq(stack_id)
-		.where('resource_name').eq(res_name)
-		.delete( cb )
+	async.waterfall([
+
+		// delete the bucket
+		function( cb ) {
+
+			s3.deleteBucket({ Bucket: properties.BucketName }, function(err, data) {
+				cb(err)
+			});
+
+		},
+
+
+		// delete the resource from db
+		function( cb ) {
+			DynamoDB
+				.table('cloudformation_resources')
+				.where('stack_id').eq(stack_id)
+				.where('resource_name').eq(res_name)
+				.delete( function(err) {
+					cb(err)
+				} )
+		},
+	], function(err) {
+		// if err, mark resource as failed delete
+
+		cb(err)
+	})
+
 }
+
