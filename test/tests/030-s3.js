dc8254a24b533a28844969ce43ad33152eb63053
databank <adrian@databank.ro>
2019-04-12 13:06:32 +0300

    update

--- test/tests/030-s3.js
+++ test/tests/030-s3.js
@@ -27,7 +27,41 @@ Resources:
 		});
 	})
 
+
+	it('CreateStack with existing bucket (should fail)', function(done) {
+
+		var params = {
+			StackName: 'stack2',
+			TemplateBody: `
+AWSTemplateFormatVersion: 2010-09-09
+Resources:
+    MyTable:
+        Type: AWS::S3::Bucket
+        Properties:
+            BucketName: my-bucket
+`,
+		};
+		cloudformation.createStack(params, function(err, data) {
+			
+			if (err && (err.code === 'BucketAlreadyExists'))
+				return done()
+
+			if (err)
+				throw err
+
+			throw 'CreateStack with existing Bucket should have failed'
+		});
+	})
+	it('DeleteStack', function(done) {
+		cloudformation.deleteStack({ StackName: 'stack2', }, function(err, data) {
+			done()
+		});
+	})
+
+
 	// @todo: list tables, check it it is pay per request
+
+
 
 	it('DeleteStack', function(done) {
 		var params = {
