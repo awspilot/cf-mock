6ddd69ebff20fc8f69e9051afb09ef9b64db7152
databank <adrian@databank.ro>
2019-05-06 19:06:18 +0300

    update

--- test/index.js
+++ test/index.js
@@ -7,6 +7,7 @@ require('./tests/000-index.js')
 require('./tests/001-pseudoparameters.js')
 require('./tests/002-ref.js')
 require('./tests/003-base64.js')
+require('./tests/004-join.js')
 
 
 require('./tests/020-describestacks.js')
