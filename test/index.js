268850c21afd8e90640da33c1cc8bb6c5e07a17d
databank <adrian@databank.ro>
2019-05-06 17:14:46 +0300

    update

--- test/index.js
+++ test/index.js
@@ -4,11 +4,11 @@ require('./lib/index.js')
 
 require('./tests/000-index.js')
 
+require('./tests/001-pseudoparameters.js')
 require('./tests/002-ref.js')
 
 require('./tests/020-describestacks.js')
 require('./tests/021-getTemplateSummary.js')
-require('./tests/003-pseudoparameters.js')
 require('./tests/004-liststackresources.js')
 
 require('./tests/200-dynamodb.js')
