82c617ce8192403bedb245a1c6506fc508ad2ba3
databank <adrian@databank.ro>
2019-05-06 15:25:25 +0300

    update

--- test/tests/002-getTemplateSummary.js
+++ test/tests/002-getTemplateSummary.js
@@ -1,6 +1,60 @@
 
 describe('all', function () {
 
+	it('getTemplateSummary - multiple refs to pseudo parameters', function(done) {
+		var params = {
+			TemplateBody: `
+
+AWSTemplateFormatVersion: 2010-09-09
+Parameters:
+    p1:
+        Type: String
+        Default: nothing
+    p2:
+        Type: String
+        Default: nothing
+    p3:
+        Type: String
+        Default: nothing
+Resources:
+    # param1: !Ref ref_in_comment1
+    Res1:
+        Type: AWS::Mock::Res
+        Properties:
+          Region1: !Ref AWS::Region
+          p1: !Ref p1
+          p2: 
+            Ref: p2
+          # param1 !Ref ref_in_comment2
+          nasty_list:
+              - !Ref AWS::Region
+              - !Ref 
+                  AWS::Region
+              - !Ref p1
+              - !Ref p2
+              - !Ref p3
+`,
+		};
+		cloudformation.getTemplateSummary(params, function(err, data) {
+			if (err)
+				throw err
+
+			done()
+		});
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
 	it('getTemplateSummary - !Ref to parameter in comment', function(done) {
 		var params = {
 			TemplateBody: `
