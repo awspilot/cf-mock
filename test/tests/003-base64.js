fc3fe15d9f97abc38f02f241a10c5b9db32ae3de
databank <adrian@databank.ro>
2019-05-07 13:44:16 +0300

    support for Fn::Base64

--- test/tests/003-base64.js
+++ test/tests/003-base64.js
@@ -1,5 +1,5 @@
 describe('!Base64', function () {
-	it('CreateStack', function(done) {
+	it('!Base64 string', function(done) {
 
 		var params = {
 			StackName: 'STRING_VALUE',
@@ -51,5 +51,65 @@ Resources:
 		});
 	})
 
+
+
+
+
+	it('Fn::Base64: !Base64 string', function(done) {
+
+		var params = {
+			StackName: 'STRING_VALUE',
+			TemplateBody: `
+AWSTemplateFormatVersion: 2010-09-09
+Resources:
+    MyTable:
+        Type: AWS::DynamoDB::Table
+        Properties:
+            TableName: 
+                Fn::Base64: !Base64 mytable
+                # Fn::Base64: !Sub |
+                #       multi
+                #       line
+                #       string
+            BillingMode: PAY_PER_REQUEST
+            AttributeDefinitions:
+                -
+                  AttributeName: field
+                  AttributeType: S
+            KeySchema:
+                -
+                  AttributeName: field
+                  KeyType: HASH
+`,
+		};
+		cloudformation.createStack(params, function(err, data) {
+			if (err)
+				throw err;
+
+			setTimeout(function() {
+				done()
+			}, 2000)
+		});
+	})
+	it('ListStackResources', function(done) {
+		cloudformation.listStackResources({ StackName: 'STRING_VALUE', }, function(err, data) {
+	
+			if (err)
+				throw err;
+	
+			if (data.StackResourceSummaries[0].PhysicalResourceId !==  Buffer.from(Buffer.from('mytable').toString('base64')).toString('base64')   )
+				throw 'base64 failed';
+	
+			done()
+		});
+	})
+	it('DeleteStack', function(done) {
+		cloudformation.deleteStack({ StackName: 'STRING_VALUE', }, function(err, data) {
+			if (err)
+				throw err;
+
+			done()
+		});
+	})
 
 })
