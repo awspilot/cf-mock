4ad5ec11064ce531389efbf3737142f4242f8ca7
databank <adrian@databank.ro>
2019-05-07 14:22:26 +0300

    tests for \!Select

---
+++ test/tests/008-select.js
@@ -0,0 +1,138 @@
+
+describe('!Select', function () {
+	it('!Select [ idx, [ x, y, z ] ]', function(done) {
+
+		var params = {
+			StackName: 'STRING_VALUE',
+			TemplateBody: `
+AWSTemplateFormatVersion: 2010-09-09
+Parameters:
+    tbl_name:
+        Type: String
+        Default: my_tbl
+Resources:
+    MyTable:
+        Type: AWS::DynamoDB::Table
+        Properties:
+            TableName: !Select [ 1, [ !Ref tbl_name, !Ref AWS::StackName ] ]
+            BillingMode: PAY_PER_REQUEST
+            AttributeDefinitions:
+                -
+                  AttributeName: field
+                  AttributeType: S
+            KeySchema:
+                -
+                  AttributeName: field
+                  KeyType: HASH
+`,
+			};
+		cloudformation.createStack(params, function(err, data) {
+
+			if (err)
+				throw err;
+		
+			setTimeout(function() {
+				done()
+			}, 2000)
+		});
+	})
+	it('ListStackResources', function(done) {
+		cloudformation.listStackResources({ StackName: 'STRING_VALUE', }, function(err, data) {
+	
+			if (err)
+				throw err;
+	
+			if ( data.StackResourceSummaries[0].PhysicalResourceId !== 'STRING_VALUE' )
+				throw '!Select failed'
+	
+			done()
+		});
+	})
+	it('DeleteStack', function(done) {
+		cloudformation.deleteStack({ StackName: 'STRING_VALUE', }, function(err, data) {
+			if (err)
+				throw err;
+	
+			done()
+		});
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	it('!Join ["_", [ !Ref tbl_name, !Select [ 1, [ !Ref tbl_name, !Ref AWS::StackName ] ] ] ]', function(done) {
+
+		var params = {
+			StackName: 'STRING_VALUE',
+			TemplateBody: `
+AWSTemplateFormatVersion: 2010-09-09
+Parameters:
+    tbl_name:
+        Type: String
+        Default: my_tbl
+Resources:
+    MyTable:
+        Type: AWS::DynamoDB::Table
+        Properties:
+            TableName: !Join [ '_', [ !Ref tbl_name, !Select [ 1, [ !Ref tbl_name, !Ref AWS::StackName ] ] ] ]
+            BillingMode: PAY_PER_REQUEST
+            AttributeDefinitions:
+                -
+                  AttributeName: field
+                  AttributeType: S
+            KeySchema:
+                -
+                  AttributeName: field
+                  KeyType: HASH
+`,
+			};
+		cloudformation.createStack(params, function(err, data) {
+
+			if (err)
+				throw err;
+		
+			setTimeout(function() {
+				done()
+			}, 2000)
+		});
+	})
+	it('ListStackResources', function(done) {
+		cloudformation.listStackResources({ StackName: 'STRING_VALUE', }, function(err, data) {
+	
+			if (err)
+				throw err;
+	
+			if ( data.StackResourceSummaries[0].PhysicalResourceId !== 'my_tbl_STRING_VALUE' )
+				throw '!Select failed'
+	
+			done()
+		});
+	})
+	it('DeleteStack', function(done) {
+		cloudformation.deleteStack({ StackName: 'STRING_VALUE', }, function(err, data) {
+			if (err)
+				throw err;
+	
+			done()
+		});
+	})
+
+
+
+
+})
+
